37666f6c21992799d14a1cd4e90b0e997f05b411
John Abramsson <John.abramsson@gmail.com>
2018-04-10 11:06:58 +0200

    Add post functionality to blogpostwebserver.rkt Add testfile for send/suspend/dispatch Add testfile2 for send/suspend/dispatch

--- testCode/blogpostwebserver.rkt
+++ testCode/blogpostwebserver.rkt
@@ -14,7 +14,25 @@
 ; Consumes a request, and produces a page that displays all of the
 ; web content.
 (define (start request)
-  (render-blog-page BLOG request))
+  (define a-blog
+    (cond [(can-parse-post? (request-bindings request))
+           (cons (parse-post (request-bindings request))
+                 BLOG)]
+          [else
+           BLOG]))
+  (render-blog-page a-blog request))
+
+; can-parse-post?: bindings -> boolean
+; Produces true if bindings contains values for 'title and 'body.
+(define (can-parse-post? bindings)
+  (and (exists-binding? 'title bindings)
+       (exists-binding? 'body bindings)))
+
+; parse-post: bindings -> post
+; Produces a bindings, and produces a post out of the bindings.
+(define (parse-post bindings)
+  (post (extract-binding/single 'title bindings)
+        (extract-binding/single 'body bindings)))
  
 ; render-blog-page: blog request -> response
 ; Consumes a blog and a request, and produces an HTML page
@@ -22,8 +40,13 @@
 (define (render-blog-page a-blog request)
   (response/xexpr
    `(html (head (title "My Blog"))
-          (body (h1 "My Blog")
-                ,(render-posts a-blog)))))
+          (body
+           (h1 "My Blog")
+           ,(render-posts a-blog)
+           (form
+            (input ((name "title")))
+            (input ((name "body")))
+            (input ((type "submit"))))))))
  
 ; render-post: post -> xexpr
 ; Consumes a post, produces an xexpr fragment of the post.
@@ -38,3 +61,4 @@
 (define (render-posts a-blog)
   `(div ((class "posts"))
         ,@(map render-post a-blog)))
+
