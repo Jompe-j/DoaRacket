afb322df1af929086cb0bb8e61e00b61d9fde218
John Abramsson <John.abramsson@gmail.com>
2018-05-06 21:29:40 +0200

    add second question to game play. add centurySelect

--- deadOrAlive/testDoa.rkt
+++ deadOrAlive/testDoa.rkt
@@ -6,12 +6,18 @@
  (let
      ((most-certainly-almost-dead (pick-k (random (length arguably-dead)) arguably-dead)))
    (begin    
-     (println (car most-certainly-almost-dead))         
+     (println (car most-certainly-almost-dead))
      (let
          ((answer (read)))
-       (if (eq? answer (car (cdr most-certainly-almost-dead)))
+       (if (eq? answer (pick-k 0 (cdr most-certainly-almost-dead)))
            (print "Correct!")
-           (print "Nob."))))))
+           (print "Nob.")))
+     (let
+         ((answer2 (read)))
+       (if (eq? answer2 (pick-k 1 (cdr most-certainly-almost-dead)))
+           (print "Correct.")
+           (print "no!"))))))
+
 
 (define (pick-k k lst)
  (if (eq? k 0)
@@ -19,14 +25,16 @@
      (pick-k (- k 1) (cdr lst))))
 
 
+               
+
 (define arguably-dead
- '(("Hitler" yes)
-   ("Moussolini" yes)))
+ '(("Hitler" . (yes 10))
+   ("Moussolini" . (yes 10))))
 
 
 
 
-(displayln (string->jsexpr "{ \"qwe\": 123, \"zxc\": [ 4, 5, 6 ] }"))
+;; (displayln (string->jsexpr "{ \"qwe\": 123, \"zxc\": [ 4, 5, 6 ] }"))
 
 
 
