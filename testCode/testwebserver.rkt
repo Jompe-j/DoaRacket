0ed95883f355a87fbdf6669495fb87cb3385d73e
John Abramsson <John.abramsson@gmail.com>
2018-06-03 17:08:10 +0200

    Moved stand alone executable (in zip) from source code folder. Removed old training code in testCode folder from repository.

--- testCode/testwebserver.rkt
+++
@@ -1,8 +0,0 @@
-#lang web-server/insta
-
-(define (start request)
-  (response/xexpr
-   '(html
-     (head (title "Test page for test server"))
-     (body (h1 "First try at racket webserver")))))
-(no-web-browser)
